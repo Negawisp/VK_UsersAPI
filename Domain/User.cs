5c61ac331cf674c6c8b9798b4f012b6de878ab0a
Negawisp <BlindEyed@yandex.ru>
2023-05-10 02:37:01 +0300

    Data model created

---
+++ Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public int UserGroupId { get; set;}
+
+        [Required]
+        public int UserStateId{ get; set; }
+    }
+}
---
+++ Domain/UserState.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class UserState
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
